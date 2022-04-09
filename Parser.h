06c165ed24d00b83e08edfd60503e402ca05377f
Billy Bertrand <bertran.billy@gmail.com>
2022-04-09 11:56:14 -0500

    Added comments Parser.h

--- Parser.h
+++ Parser.h
@@ -1,6 +1,11 @@
-//
-// Created by pravin on 4/6/22.
-//
+/**
+ * @Author(s):      Pravin and Kassi
+ * @filename:       Parser.cpp
+ * @date:           04-05-2022
+ * @description:    This class is represents the parser of the project
+ *                  it reads JSON files and convert them into a vector
+ *                  of normalized tokens
+ */
 
 #ifndef INC_22S_FINAL_PROJ_PARSER_H
 #define INC_22S_FINAL_PROJ_PARSER_H
