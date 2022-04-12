185fed5b9d380fe4cef19622f164d8b33dfc2a93
pravin <pravinramana25@protonmail.ch>
2022-04-12 16:10:48 -0500

    propose changed AVLTree.h implementation, removing needless templates and parameters in function definition

--- AvlTree.h
+++ AvlTree.h
@@ -16,17 +16,11 @@ private:
     class AvlNode {
     public:
         T element;
-        AvlNode<T> *left;
-        AvlNode<T> *right;
+        AvlNode *left;
+        AvlNode *right;
         int height;
 
-        AvlNode(const T& the_element,
-                AvlNode<T> *lt,
-                AvlNode<T> *rt,
-                int h = 0) : element(the_element),
-                             left(lt),
-                             right(rt),
-                             height(h) {}
+        explicit AvlNode(const T &element) : element(element), left(nullptr), right(nullptr), height(0) {}
     };
 
 
@@ -34,41 +28,41 @@ private:
     void insert_node(const T &value, AvlNode *&node);
 
     /// \description    -> Search node in the AVL tree
-    T* search_node(const T &value, AvlNode *&node);
+    T *search_node(const T &value, AvlNode *&node);
 
     /// \description    -> Empties AVL tree
     void make_empty();
 
     /// \param node     -> Node of imbalance
     /// \description    -> Performs "case 1" rotation
-    void rotate_with_left_child(AvlNode<T> *& node);
+    void rotate_with_left_child(AvlNode *&node);
 
 
     /// \param node     -> Node of imbalance
     /// \description    -> Performs "case 2" rotation
-    void double_with_left_child(AvlNode<T> *& node);
+    void double_with_left_child(AvlNode *&node);
 
     /// \param node     -> Node of imbalance
     /// \description    -> Performs "case 3" rotation
-    void double_with_right_child(AvlNode<T> *& node);
+    void double_with_right_child(AvlNode *&node);
 
     /// \param node     -> Node of imbalance
     /// \description    -> Performs "case 4" rotation
-    void rotate_with_right_child(AvlNode<T> *& node);
+    void rotate_with_right_child(AvlNode *&node);
 
 
     /// \param  node    -> Node of imbalance
     /// \description    -> balances AVL tree
-    void balance(AvlNode<T> *&node);
+    void balance(AvlNode *&node);
 
     //AVL tree root node
-    AvlNode<T> *root;
+    AvlNode *root;
 public:
 
     //constructors
     AvlTree() : root(nullptr) {}
 
-    AvlTree(const AvlTree<T> &rhs);
+    AvlTree(const AvlTree<T> &tree);
 
     ~AvlTree() {
         make_empty();
@@ -76,14 +70,14 @@ public:
 
     /// \param value    -> Value to be added to AVL tree
     /// \description    -> Insert a new node into the AVL tree
-    void insert(const T& value) {
+    void insert(const T &value) {
         insert_node(value, root);
     }
 
     /// \param value    -> Element to find
     /// \return T*      -> Pointer to value or NULL
     /// \description    -> Search
-    T* search(const T& value){
+    T *search(const T &value) {
         return search_node(value, root);
     }
 };
@@ -92,12 +86,10 @@ public:
 template<typename T>
 void AvlTree<T>::insert_node(const T &value, AvlNode *&node) {
     if (node == nullptr) {
-        node = new AvlNode<T>(value, nullptr, nullptr);
-    }
-    else if (value < node->element) {
+        node = new AvlNode(value);
+    } else if (value < node->element) {
         insert_node(value, node->left);
-    }
-    else if (node->element < value) {
+    } else if (node->element < value) {
         insert_node(value, node->right);
     } else;
 
