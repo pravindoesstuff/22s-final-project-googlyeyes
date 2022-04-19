2b1badf1261d2008c127797b3ea5d0b71eb8f78f
pravin <pravinramana25@protonmail.ch>
2022-04-19 17:55:18 -0500

    Change AVL tree builder to build one single AVL tree

--- main.cpp
+++ main.cpp
@@ -9,13 +9,7 @@ int main(int argc, char **argv) {
     Porter2Stemmer::trim(search_term);
     Porter2Stemmer::stem(search_term);
     parser.parse(argv[2]);
-    std::vector<AvlTree<std::string, Article *>> pairs = parser.build_AVL_trees();
-    for (auto &pair: pairs) {
-        auto n = pair.search(search_term);
-        if (n == nullptr) continue;
-        for (auto &i: *n)
-            std::cout << i->title << '\n';
-    }
+    AvlTree<std::string, Article *> article_tree = parser.build_AVL_trees();
 //    std::vector<Article *> matches = parser.search_article_trees(search_term, pairs);
 //    std::cout << matches.size();
 //    for (Article *article: matches) std::cout << article->id << '\n';
